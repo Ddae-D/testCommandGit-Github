e2e44719b33d232bf9995b92cc053cda41d214f2
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-13 14:37:08 -0500

    agregando comentario desde git

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.write("<p id='saludo'>Hello " + "<b id='inp'>" + nom + "</b>" + ", <i> saludos! </i> </p>");
 document.write("<p> la fecha actual es :" + fecha + "</p>");
 var texto = document.getElementById("inp").style.color = "white";
-//comentario agregado desde GitHub a Git, usamos comando pull para llevar cambios a local
+//comentario agregado desde GitHub a Git, usamos comando pull para bajar cambios a local
+//comentario agregado desde Git a Github, usamos comando push para subir los cambios a remoto
