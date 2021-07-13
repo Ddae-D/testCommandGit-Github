ec4bf17aef83f064fcf25446d9b0a07e2cfcf48c
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-13 14:35:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 document.write("<p id='saludo'>Hello " + "<b id='inp'>" + nom + "</b>" + ", <i> saludos! </i> </p>");
 document.write("<p> la fecha actual es :" + fecha + "</p>");
 var texto = document.getElementById("inp").style.color = "white";
+//comentario agregado desde GitHub a Git, usamos comando pull para llevar cambios a local
